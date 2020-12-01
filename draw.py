912dce60bf352dc23c815113895bd1b589275e6f
pmalhaire <pierre.malhaire@gmail.com>
2020-12-01 09:42:26 +0100

    carousel

--- draw.py
+++ draw.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import sys
+import os
+import time
 
 
 def get_from_stdin(ncols, nrows):
@@ -38,7 +40,7 @@ def tree():
     return image
 
 
-def plot(image):
+def plot(image, pause=None):
     # Print lines in hex
     print('{', end='')
     for x in range(ncols):
@@ -57,9 +59,45 @@ def plot(image):
     plt.matshow(image)
     plt.xticks(range(ncols), col_labels)
     plt.yticks(range(nrows), row_labels)
-    plt.show()
+    if pause:
+        plt.pause(pause)
+        plt.close()
+    else:
+        plt.show()
+
+
+def readfile(filename, ncols, nrows):
+    image = list()
+    idx = nrows
+    with open(filename, 'r') as f:
+        charline = list()
+        for char in f.read():
+            if char == '\n':
+                # check line size
+                if len(charline) != ncols:
+                    raise Exception(f'invalid line count img:{image}')
+                # add line
+                image.append(charline)
+                idx -= 1
+                charline = list()
+                continue
+            # end the image
+            if idx <= 0:
+                break
+            # add value in line
+            charline.append(int(char))
+    return image
+
+
+def plotdir(path):
+    for file in os.scandir(path):
+        if file.is_file() and file.name.endswith('.txt'):
+            image = readfile(file.name, 8, 8)
+            print(f'file:{file.name}')
+            plot(image, pause=1)
 
 
 if __name__ == "__main__":
-    image = get_from_stdin(ncols, nrows)
-    plot(image)
+    # image = get_from_stdin(ncols, nrows)
+    # plot(image)
+    plotdir('.')
